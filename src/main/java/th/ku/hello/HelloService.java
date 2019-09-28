bc4de488c10f2e091d94f7c19539611b445f54a7
Surattana <surattana.1998@gmail.com>
2019-09-28 22:28:47 +0700

    add spring boot basic

---
+++ src/main/java/th/ku/hello/HelloService.java
@@ -0,0 +1,21 @@
+package th.ku.hello;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/greeting"})
+public class HelloService {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "<html><h1>Hello</h1></html>";
+    }
+
+    @GetMapping("/hi")
+    public String hi(){
+        return "Hi";
+    }
+}
